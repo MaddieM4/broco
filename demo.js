552ef988b7fff967bd6c10ab49644c2290e88708
Philip Horger <campadrenalin@gmail.com>
2014-11-01 15:20:41 -0700

    Early visual demos

--- demo.js
+++ demo.js
@@ -1,3 +1,11 @@
-define([], function() {
-    console.log("Demo.js loaded");
+require.config({
+    paths: {
+        'broco': 'src'
+    }
+});
+
+define(['broco/ui'], function(ui) {
+    broco = new ui('#console');
+    broco.prompt.value = "Living ship";
+    broco.prompt.display();
 });
