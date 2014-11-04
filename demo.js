d7ba83c3f498299a3aed728b46f14a66e23452a6
Philip Horger <campadrenalin@gmail.com>
2014-11-03 16:51:06 -0800

    Close #1: Finishing touch is the help module

--- demo.js
+++ demo.js
@@ -8,7 +8,14 @@ define(['broco/logic/console', 'broco/ui/core'], function(BrocoConsole, BrocoUI)
     var console = new BrocoConsole();
     var ui = new BrocoUI('#console', console);
     // console.depend('broco/modules/', ['load']);
-    console.depend('broco/modules/', ['motd','load'], function() {
+    console.depend('broco/modules/', ['motd','load','help'], function() {
+        console.modules.help.tutorials.getting_started = [
+            'You can load modules at run-time. Try this:',
+            '$> load broco/modules fullscreen',
+            'Now you have access to the `fullscreen` module. Give it a try!',
+            '$> fullscreen',
+            'Modules are just commands. Most projects using broco for their own purposes, will provide their own modules.'
+        ];
         ui.process('motd');
     });
 });
